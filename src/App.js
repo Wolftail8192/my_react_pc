3907bbef02235bdea8d3df8d4a2ab15f31647657
Wolftail8192 <4420299@gmail.com>
2022-01-16 19:05:48 +0200

    base2.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Users from './Components/Users/Users';
 import UserDetails from './Components/UserDetails/UserDetails';
 import css from './App.module.css';
@@ -6,15 +6,22 @@ import Posts from "./Components/Posts/Posts";
 
 
 const App = () => {
+
+    const [user,setUser] = useState(null);
+
+    const getUser = (user) => {
+
+    }
     return (
         <div>
           <div className={css.wrap}>
-              <Users/>
+              <Users getUser={{getUser}}/>
               <UserDetails/>
           </div>
               <Posts/>
         </div>
     );
 };
+
 
 export default App;
